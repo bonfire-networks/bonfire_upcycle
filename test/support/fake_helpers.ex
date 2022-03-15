664b195e47fe20e33887a9d350c0e4923c84cbfd
Hunter Turcin <huntertur@gmail.com>
2022-03-15 11:06:34 -0500

    Use Bonfire.Me's fakes

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -7,15 +7,4 @@ defmodule Bonfire.Upcycle.Test.FakeHelpers do
 
   import Bonfire.Upcycle.Integration
 
-  def fake_account!(attrs \\ %{}) do
-    cs = Accounts.signup_changeset(Fake.account(attrs))
-    assert {:ok, account} = repo().insert(cs)
-    account
-  end
-
-  def fake_user!(%Account{}=account, attrs \\ %{}) do
-    assert {:ok, user} = Users.create(Fake.user(attrs), account)
-    user
-  end
-
 end
