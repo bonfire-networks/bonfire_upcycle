6d5194bb37ae662af14f5ebfd2cc59477dfd2c19
Hunter Turcin <huntertur@gmail.com>
2022-03-15 11:09:33 -0500

    Import instead of alias Bonfire.Me.Fake

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.Upcycle.Test.FakeHelpers do
 
   alias Bonfire.Data.Identity.Account
-  alias Bonfire.Me.Fake
+  import Bonfire.Me.Fake
   alias Bonfire.Me.Identity.{Accounts, Users}
   import ExUnit.Assertions
 
