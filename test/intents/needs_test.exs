c2ffa97d802d65388aae9dcbde95f8bc2be85c21
Amish Verma <vermaa@msoe.edu>
2022-04-01 15:40:10 +0000

    Merge Intents tests to main

--- test/intents/needs_test.exs
+++ test/intents/needs_test.exs
@@ -1,6 +1,6 @@
 defmodule Bonfire.Upcycle.NeedsTest do
   use Bonfire.Upcycle.ConnDataCase
-
+  import Bonfire.Common.Simulation
   import ValueFlows.Simulate
   import ValueFlows.Test.Faking
   import Bonfire.Me.Fake
@@ -8,9 +8,21 @@ defmodule Bonfire.Upcycle.NeedsTest do
 
   import Phoenix.LiveViewTest
 
-  @path "/upcycle/discover"
+  @discover "/upcycle/discover"
+  @myneeds "/upcycle/my-needs"
+  @schema Bonfire.API.GraphQL.Schema
 
-  test "displays need" do
+  test "create a new need with attributes" do
+    user = fake_agent!()
+    q = create_need_mutation(fields: [receiver: [:id]])
+    conn = user_conn(user)
+    vars = %{intent: intent_input()}
+    assert intent = grumble_post_key(q, conn, :create_need, vars)["intent"]
+    assert_intent(intent)
+    assert intent["receiver"]["id"] == user.id
+  end
+
+  test "displays need on discover" do
     agent = fake_agent!()
     account = fake_account!()
     user = fake_user!(account)
@@ -26,7 +38,29 @@ defmodule Bonfire.Upcycle.NeedsTest do
 
     conn = conn(user: user, account: account)
 
-    {:ok, view, html} = live(conn, @path)
+    {:ok, view, html} = live(conn, @discover)
+
+    assert has_need?(view, "Lumber")
+    refute has_need?(view, "Steel")
+  end
+
+  test "displays need on my needs" do
+    agent = fake_agent!()
+    account = fake_account!()
+    user = fake_user!(account)
+
+    attrs = %{
+      has_point_in_time: nil,
+      has_end: nil,
+      provider: nil,
+      name: "Lumber"
+    }
+
+    assert {:ok, intent} = Intents.create(agent, intent(attrs))
+
+    conn = conn(user: user, account: account)
+
+    {:ok, view, html} = live(conn, @myneeds)
 
     assert has_need?(view, "Lumber")
     refute has_need?(view, "Steel")
--- test/intents/offers_test.exs
+++ test/intents/offers_test.exs
@@ -1,6 +1,6 @@
 defmodule Bonfire.Upcycle.OffersTest do
   use Bonfire.Upcycle.ConnDataCase
-
+  import Bonfire.Common.Simulation
   import ValueFlows.Simulate
   import ValueFlows.Test.Faking
   import Bonfire.Me.Fake
@@ -8,9 +8,22 @@ defmodule Bonfire.Upcycle.OffersTest do
 
   import Phoenix.LiveViewTest
 
-  @path "/upcycle/discover"
+  @schema Bonfire.API.GraphQL.Schema
 
-  test "displays offer" do
+  @discover "/upcycle/discover"
+  @myoffers "/upcycle/my-offers"
+
+  test "creates a new offer with attributes" do
+    user = fake_agent!()
+    q = create_offer_mutation(fields: [provider: [:id]])
+    conn = user_conn(user)
+    vars = %{intent: intent_input()}
+    assert intent = grumble_post_key(q, conn, :create_offer, vars)["intent"]
+    assert_intent(intent)
+    assert intent["provider"]["id"] == user.id
+  end
+
+  test "displays offer on discover" do
     agent = fake_agent!()
     account = fake_account!()
     user = fake_user!(account)
@@ -28,7 +41,32 @@ defmodule Bonfire.Upcycle.OffersTest do
 
     conn = conn(user: user, account: account)
 
-    {:ok, view, _html} = live(conn, @path)
+    {:ok, view, _html} = live(conn, @discover)
+
+    assert has_offer?(view, test_name)
+    refute has_offer?(view, "Definitely not the name")
+  end
+
+
+  test "displays offer on my offers" do
+    agent = fake_agent!()
+    account = fake_account!()
+    user = fake_user!(account)
+
+    test_name = "Volunteer Hours"
+
+    attrs = %{
+      has_point_in_time: nil,
+      has_end: nil,
+      provider: agent.id,
+      name: test_name
+    }
+
+    assert {:ok, intent} = Intents.create(agent, intent(attrs))
+
+    conn = conn(user: user, account: account)
+
+    {:ok, view, _html} = live(conn, @myoffers)
 
     assert has_offer?(view, test_name)
     refute has_offer?(view, "Definitely not the name")
