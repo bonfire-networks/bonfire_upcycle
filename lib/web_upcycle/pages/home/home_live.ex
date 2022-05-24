013a6b9bb20c74107b5d602e709cd3ec9462379d
Mayel de Borniol <git@mayel.space>
2022-05-24 16:04:37 +1200

    handle

--- lib/web_upcycle/pages/home/home_live.ex
+++ lib/web_upcycle/pages/home/home_live.ex
@@ -136,9 +136,12 @@ defmodule Bonfire.Upcycle.Web.HomeLive do
   end
 
   def handle_params(params, uri, socket) do
-    undead_params(socket, fn ->
-      do_handle_params(params, uri, socket)
-    end)
+    # poor man's hook I guess
+    with {_, socket} <- Bonfire.UI.Common.LiveHandlers.handle_params(params, uri, socket) do
+      undead_params(socket, fn ->
+        do_handle_params(params, uri, socket)
+      end)
+    end
   end
 
   @graphql """
