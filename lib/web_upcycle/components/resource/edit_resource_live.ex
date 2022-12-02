1d9f9701019f2b4898e0684f72f76699168c7333
Mayel de Borniol <git@mayel.space>
2022-12-02 15:20:56 +1300

    prop

--- lib/web_upcycle/components/resource/edit_resource_live.ex
+++ lib/web_upcycle/components/resource/edit_resource_live.ex
@@ -6,7 +6,7 @@ defmodule Bonfire.Upcycle.Web.EditResourceLive do
   # prop textarea_class, :css_class, required: false
   # # unused but workaround surface "invalid value for property" issue
   # prop textarea_container_class, :css_class
-  # prop to_boundaries, :list, default: nil
+  # prop to_boundaries, :list, default: []
   # prop open_boundaries, :boolean, default: false
 
   prop action, :any, default: "raise"
