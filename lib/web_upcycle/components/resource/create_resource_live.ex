d769fc800728e105e637d2644ef2e28a1fb8ff58
Mayel de Borniol <git@mayel.space>
2022-12-03 13:59:22 +1300

    to_boundaries should be nillable

--- lib/web_upcycle/components/resource/create_resource_live.ex
+++ lib/web_upcycle/components/resource/create_resource_live.ex
@@ -6,7 +6,7 @@ defmodule Bonfire.Upcycle.Web.CreateResourceLive do
   prop textarea_class, :css_class, required: false
   # unused but workaround surface "invalid value for property" issue
   prop textarea_container_class, :css_class
-  prop to_boundaries, :list, default: []
+  prop to_boundaries, :any, default: nil
   prop open_boundaries, :boolean, default: false
 
   prop action, :string, default: "raise"
