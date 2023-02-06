54d582769e3c25f1a2a6fdee32a9db7ab8c7cb1f
Mayel de Borniol <git@mayel.space>
2023-02-07 12:00:38 +1300

    smart_input_opts

--- lib/web_upcycle/components/transfer/create_transfer_live.ex
+++ lib/web_upcycle/components/transfer/create_transfer_live.ex
@@ -2,7 +2,7 @@ defmodule Bonfire.Upcycle.Web.CreateTransferLive do
   use Bonfire.UI.Common.Web, :stateless_component
   import Bonfire.Upcycle.Integration
 
-  prop smart_input_opts, :any, default: nil
+  prop smart_input_opts, :list, default: []
   prop textarea_class, :css_class, required: false
   # unused but workaround surface "invalid value for property" issue
   prop textarea_container_class, :css_class
