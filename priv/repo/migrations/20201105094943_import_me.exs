c538da61691c26198293d6456219b93e3ebe651c
Mayel de Borniol <git@mayel.space>
2023-02-20 13:56:47 +1300

    moduledoc

--- priv/repo/migrations/20201105094943_import_me.exs
+++ priv/repo/migrations/20201105094943_import_me.exs
@@ -1,4 +1,5 @@
-defmodule Bonfire.Upcycle.Repo.Migrations.ImportMe do
+defmodule Bonfire.Upcycle.Repo.Migrations.ImportMe  do
+  @moduledoc false
   use Ecto.Migration
 
   import Bonfire.Upcycle.Migration
