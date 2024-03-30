c95811c7bc154355aed458c70a60580e3e43f4a9
Mayel de Borniol <git@mayel.space>
2024-03-30 13:47:31 +0000

    fix

--- priv/repo/migrations/20201105094943_import_me.exs
+++
@@ -1,9 +0,0 @@
-defmodule Bonfire.Upcycle.Repo.Migrations.ImportMe do
-  @moduledoc false
-  use Ecto.Migration
-
-  import Bonfire.Upcycle.Migration
-  # accounts & users
-
-  def change, do: migrate_me
-end
